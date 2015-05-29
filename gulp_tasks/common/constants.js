695bdb5ceb84d3a463a3bfb399380bd625b9fe8a
Avi Haiat <thaiat@ipelia.fr>
2015-05-29 05:34:30 +0300

    feat(app): Add gulp tasks and dot files

---
+++ gulp_tasks/common/constants.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var path = require('path');
+
+module.exports = function() {
+    var cwd = process.env.INIT_CWD || '';
+    var clientFolder = 'client'; // the source file folder
+    var defaultTarget = 'app'; // the name of the app that corresponds to index.html
+    var constants = {
+        appname: 'loopback-connector-rethinkdbdash',
+        cwd: cwd,
+        defaultTarget: defaultTarget,
+        targetName: '{{targetName}}',
+        targetSuffix: '{{targetSuffix}}',
+        mode: '{{mode}}',
+        clientFolder: clientFolder,
+        repository: 'https://github.com/Yoobic/loopback-connector-rethinkdbdash',
+        versionFiles: ['./package.json', './bower.json', './' + clientFolder + '/config*.xml'],
+        growly: {
+            notify: false,
+            successIcon: path.join(cwd, 'node_modules/karma-growl-reporter/images/success.png'),
+            failedIcon: path.join(cwd, 'node_modules/karma-growl-reporter/images/failed.png')
+        },
+        lint: [
+            './*.js', 'gulpfile.js', './gulp_tasks/**/*.js', './test/**/*.js'
+        ],
+        mocha: {
+            libs: ['./*.js', '!gulpfile.js'],
+            tests: ['./test/**/*.js'],
+            globals: 'test/helpers/globals.js',
+            timeout: 5000
+        },
+        dist: {
+            distFolder: './dist/{{targetName}}/{{mode}}'
+        }
+    };
+
+    return constants;
+};
---
+++ gulp_tasks/tasks/test.js
@@ -0,0 +1,42 @@
+'use strict';
+var gulp = require('gulp');
+var runSequence = require('run-sequence');
+var $ = require('gulp-load-plugins')();
+var mocha = $.mocha;
+var istanbul = $.istanbul;
+var constants = require('../common/constants')();
+
+gulp.task('mocha', 'Runs mocha unit tests.', function() {
+    process.env.NODE_ENV = 'mocha';
+
+    return gulp.src(constants.mocha.libs)
+        .pipe(istanbul({
+            includeUntested: true
+        }))
+        .pipe(istanbul.hookRequire()) // Force `require` to return covered files
+        .on('finish', function() {
+            gulp.src(constants.mocha.tests)
+                .pipe(mocha({
+                    reporter: 'spec',
+                    globals: constants.mocha.globals,
+                    timeout: constants.mocha.timeout
+                }))
+                .on('error', function(err) {
+                    console.log(err.toString());
+                })
+                .pipe(istanbul.writeReports({
+                    reporters: ['lcov', 'json', 'text', 'text-summary', 'cobertura']
+                }))
+                .once('end', function() {
+                    process.exit();
+                });
+        });
+});
+
+gulp.task('test', 'Runs all the tests.', function(done) {
+    runSequence(
+        'lint',
+        'mocha',
+        done
+    );
+});
